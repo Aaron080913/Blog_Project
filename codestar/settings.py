047748f96c1079750cd68cbafae185c78b99bea5
Aaron Jones <aaronjjj95@aol.com>
2024-04-22 21:51:43 +0000

    add changes

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-aaron080913-blogproject-1ewk6niisb7.ws-eu110.gitpod.io',
  '.herokuapp.com']
